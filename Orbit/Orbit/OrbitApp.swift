869fad154de69385256b1168394a398f00290e16
OJS26 <oliverstrachan_01@hotmail.co.uk>
2026-03-18 12:20:22 +0000

    Initial Setu[

---
+++ Orbit/Orbit/OrbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitApp.swift
+//  Orbit
+//
+//  Created by OJ Strachan on 18/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct OrbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
